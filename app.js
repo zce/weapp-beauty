a016c48304eb288eadfbef8ddb133eb8b94d5c98
zce <w@zce.me>
2019-06-11 12:06:38 +0800

    feat: initial commit

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 App({
+  // /**
+  //  * 全局配置信息
+  //  */
+  // config: {
+  //   apiUrl: 'https://api.ai.qq.com/fcgi-bin/face/face_detectface',
+  //   apiAppId: '<your app id>',
+  //   apiAppSecret: '<your app secret>'
+  // },
 
-  /**
-   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
-   */
-  onLaunch: function () {
-    
-  },
+  // /**
+  //  * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+  //  */
+  // onLaunch () {
+  //   console.log('app on launch')
+  // },
 
-  /**
-   * 当小程序启动，或从后台进入前台显示，会触发 onShow
-   */
-  onShow: function (options) {
-    
-  },
+  // /**
+  //  * 当小程序启动，或从后台进入前台显示，会触发 onShow
+  //  */
+  // onShow (options) {
+  //   console.log('app on show', options)
+  // },
 
-  /**
-   * 当小程序从前台进入后台，会触发 onHide
-   */
-  onHide: function () {
-    
-  },
+  // /**
+  //  * 当小程序从前台进入后台，会触发 onHide
+  //  */
+  // onHide () {
+  //   console.log('app on hide')
+  // },
 
-  /**
-   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
-   */
-  onError: function (msg) {
-    
-  }
+  // /**
+  //  * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+  //  */
+  // onError (msg) {
+  //   console.log('app on error', msg)
+  // }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,71 +1,114 @@
+// const { apiUrl, apiAppId, apiAppSecret } = getApp().config
+
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    // https://picsum.photos/1080/1920/?image=302
-    image: '/assets/background.jpg',
+    // https://images.unsplash.com/photo-1560042289-7951ad5bfcf5?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=750&h=1334&fit=crop&ixid=eyJhcHBfaWQiOjF9
+    image: '/assets/placeholder.png',
+    showTips: false,
     result: null
   },
 
+  /**
+   * 分析照片
+   */
   detectImage (src) {
-    wx.showLoading({ title: '分析中...' })
-
     const that = this
 
+    // 取消之前的结果显示
+    that.setData({ result: null })
+
+    // loading
+    wx.showLoading({ title: '分析中...' })
+
+    // 将图片上传至 AI 服务端点
     wx.uploadFile({
       url: 'https://ai.qq.com/cgi-bin/appdemo_detectface',
-      filePath: src,
       name: 'image_file',
+      filePath: src,
       success (res) {
+        // 解析 JSON
         const result = JSON.parse(res.data)
-        
-        // 检测失败
-        if (result.ret !== 0) {
-          wx.showToast({ icon: 'none', title: '找不到你的小脸蛋喽' })
-          return false
+
+        if (result.ret === 0) {
+          // 成功获取分析结果
+          that.setData({ result: result.data.face[0] })
+        } else {
+          // 检测失败
+          wx.showToast({ icon: 'none', title: '找不到你的小脸蛋喽～' })
         }
 
-        that.setData({ result: result.data.face[0] })
+        // end loading
         wx.hideLoading()
       }
     })
   },
 
+  /**
+   * 获取照片
+   */
   getImage (type = 'camera') {
     const that = this
+    
+    // 调用系统 API 选择或拍摄照片
     wx.chooseImage({
-      sizeType: ['original'],
-      sourceType: [type],
-      success(res) {
+      sourceType: [type], // camera | album
+      sizeType: ['compressed'], // original | compressed
+      count: 1,
+      success (res) {
+        // 取照片对象
         const image = res.tempFiles[0]
 
         // 图片过大
         if (image.size > 1024 * 1000) {
-          wx.showToast({ icon: 'none', title: '图片过大, 请重新拍张小的！' })
-          return false
+          return wx.showToast({ icon: 'none', title: '图片过大, 请重新拍张小的！' })
         }
 
+        // 显示到界面上
         that.setData({ image: image.path })
+
+        // 分析检测人脸
         that.detectImage(image.path)
       }
     })
+
+    // 关闭 Tips 显示
+    this.setData({ showTips: false })
   },
 
-  handleCamera () {
-    this.getImage()
+  /**
+   * 按钮事件处理函数
+   */
+  handleClick (e) {
+    if (e.type === 'tap') {
+      // 短按拍照为拍摄照片
+      this.getImage()
+    } else if (e.type === 'longpress') {
+      // 长按拍照为选择照片
+      this.getImage('album')
+    }
   },
 
-  handleChoose () {
-    this.getImage('album')
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad (options) {
+    const isUsed = wx.getStorageSync('is_used')
+    if (isUsed) return
+    // 第一次使用显示 Tips
+    this.setData({ showTips: true })
+    // 并记住用使用过了
+    wx.setStorageSync('is_used', true)
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage () {
-    if (this.data.result) {
-      return { title: `刚刚测了自己的颜值为【${this.data.result.beauty}】你也赶紧来试试吧！` }
-    }
+    if (!this.data.result) return
+    // 如果有分析结果，则分享
+    return { title: `刚刚测了我的颜值「${this.data.result.beauty}」你也赶紧来试试吧！` }
   }
 })
